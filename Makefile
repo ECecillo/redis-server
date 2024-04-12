75ca22902cdf59fb45f531c2be96cabbbd05eccb
ECecillo <enzocecillon@MBP-de-Enzo.lan>
2024-04-12 20:33:47 +0200

    feat: refactor to object server config and handle client connections

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: run
+
+run:
+	./spawn_redis_server.sh
--- app/server.go
+++ app/server.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
+type Server struct {
+	addr string
+	port int
+}
+
+func handleClientConnection(clientConnection net.Conn) {
+	defer clientConnection.Close()
+
+	// Read data
+	buf := make([]byte, 1024)
+	dataLength, err := clientConnection.Read(buf)
+	if err != nil {
+		return
+	}
+	log.Println("Received Data", buf[:dataLength])
+
+	answer := "+PONG\r\n"
+	clientConnection.Write([]byte(answer))
+}
+
 func main() {
+	serverConfig := Server{addr: "0.0.0.0", port: 6379}
+	server := fmt.Sprintf("%s:%d", serverConfig.addr, serverConfig.port)
+
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", server)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+
+	fmt.Println("Server listening on ", listener.Addr())
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		handleClientConnection(connection)
 	}
 }
